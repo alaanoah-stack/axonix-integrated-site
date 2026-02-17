0d2141ffe6495081f22fe3e52ef85dde939cb0af
alaanoah-stack <alaanoah@gmail.com>
2026-02-17 15:19:40 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
-// @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  output: 'server',
+  adapter: cloudflare(),
+  integrations: [
+    tailwind(), 
+    mdx(), 
+    sitemap()
+  ],
 });
