b75fa28a5868c9524d133544a4f3aca6813032d1
alaanoah-stack <alaanoah@gmail.com>
2026-02-18 10:20:22 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [sitemap()],
+  output: "static",
 });
