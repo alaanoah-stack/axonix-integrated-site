5e51a0ee6217a9996f54b98785d81c0d1a835209
alaanoah-stack <alaanoah@gmail.com>
2026-02-18 10:03:43 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  // Setting output to 'static' makes it faster and avoids the "Function" error
-  output: 'static', 
   integrations: [tailwind()],
 });
