22a85b141308872d718618ba4cb7618af02259cc
alaanoah-stack <alaanoah@gmail.com>
2026-04-23 14:59:00 -0400

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  output: "static",
-  integrations: [tailwind(), mdx(), sitemap()],
-});
